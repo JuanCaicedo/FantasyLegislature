1032ce9f299cc505fe9ea71a5088bae2b5b6a932
Juan Caicedo <juan@spanishdict.com>
2016-02-10 22:06:36 -0500

    static list of senators

--- app.elm
+++ app.elm
@@ -1,26 +1,41 @@
 module FantasySenate where
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import StartApp
 import Effects exposing (Effects)
 import Signal exposing (Address)
+import List
 
 type alias Action = {}
 
 
-type alias Model = {}
+type alias Model =
+  { senators: List String }
 
 
 view : Address action -> Model -> Html
 view action model =
-  h1 [] [ text "test" ]
+  div
+    []
+    [ h1 [] [ text "test" ]
+    , ul [ class "senators-list" ]
+         (List.map senatorListItem model.senators )
+    ]
 
+senatorListItem senator =
+  li [] [ text senator ]
 
 update : Action -> Model -> ( Model, Effects Action )
 update action model = ( model, Effects.none )
 
 initialModel : Model
-initialModel = {}
+initialModel =
+  { senators =
+      [ "Ron Wyden"
+      , "Jeff Merkeley"
+      ]
+  }
 
 app =
   StartApp.start
